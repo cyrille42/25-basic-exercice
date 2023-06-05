2f4507897f05b64eafc3c4d046aafbd037a2f749
cyrille42 <cyrillegay1@gmail.com>
2023-06-05 12:05:14 +0700

    feature: add user view and serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,1 +1,19 @@
 from rest_framework import serializers
+
+
+class UserLoginSerializer(serializers.Serializer):
+    """
+    Serializer for user login.
+    """
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        if username and password:
+            # We can put special specification here
+            return attrs
+        else:
+            raise serializers.ValidationError('Invalid credentials')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserLoginSerializer
+
+
+class UserLoginView(generics.GenericAPIView):
+    """
+    API view for user login.
+    """
+    serializer_class = UserLoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+
+        return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
