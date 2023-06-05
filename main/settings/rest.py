15b6e0ca2b29c70c3b9ee28743961779d11f3a60
cyrille42 <cyrillegay1@gmail.com>
2023-06-05 13:21:33 +0700

    config: add login config and view url

--- main/settings/rest.py
+++ main/settings/rest.py
@@ -2,7 +2,7 @@ from main.jsonenv import env
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         # "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": [
--- main/urls/api.py
+++ main/urls/api.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
 from rest_framework import routers
-
+from sales.views import SaleCreateView, SaleUpdateView, SaleListView, ArticleListView
+from users.views import UserLoginView
 
 router = routers.DefaultRouter(trailing_slash=False)
 
@@ -11,6 +12,11 @@ urlpatterns = [
         include(
             [
                 path("", include(router.urls)),
+                path("login/", UserLoginView.as_view(), name="api-login"),
+                path('sales/', SaleCreateView.as_view(), name='sale-create'),
+                path('sales/<int:pk>/', SaleUpdateView.as_view(), name='sale-update'),
+                path('sales/list/', SaleListView.as_view(), name='sale-list'),
+                path('articles/list/', ArticleListView.as_view(), name='article-list'),
             ]
         ),
     )
